4463987ece3ebea7932ca9d36c52caaafed73b6c
coeurisa <kyriane66@gmail.com>
2021-08-19 21:47:14 +0200

    coucou

---
+++ components/Menu/Bar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sidebar">
+        <div class="sidebar-backdrop" @click="closeSidebarPanel" v-if="isPanelOpen"></div>
+        <transition name="slide">
+            <div v-if="isPanelOpen"
+                 class="sidebar-panel">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+<script>
+    
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="w-full h-full bg-blue-300 bg-gradient-to-b">
+  <div class="w-full h-full bg-gradient-to-b from-blue-300 via-green-200 to-purple-300">
     <header class="fixed flex items-center justify-between w-full p-2 border-b-4 border-red-200 bg-gradient-to-b from-blue-400 h-28">
       <h1 class="w-full text-6xl text-center text-green-800 transition duration-500 ease-in-out cursor-pointer hover:text-white">
         WeatherApp
       </h1>
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-20 h-20" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-      </svg>
+      <Burger/>
     </header>
+    <Bar>
+     <ul class="sidebar-panel-nav">
+       <li><a href="#home">Home</a></li>
+       <li><a href="#about">About</a></li>
+       <li><a href="#contact">Contact</a></li>
+     </ul>
+   </Bar>
     <div class="grid h-screen grid-cols-4 pt-28 justify-items-stretch">
       <nav id="scrollable" class="h-full overflow-y-scroll">
       <ul class="flex-col justify-between w-full col-span-1 ">
@@ -26,19 +31,30 @@
         </svg>
       </div>
       <div v-else class="flex-col col-span-3 pt-6 mx-6 space-y-6">
-        <h1 class="text-6xl text-center text-white"> {{cityfocused.name}} - {{region}}</h1>
+        <h1 class="text-6xl text-center text-red-500"> {{cityfocused.name}} - {{country}}</h1>
         <h2 class="text-3xl text-center text-white"> Température: {{temp}}°C </h2>
         <h2 class="text-3xl text-center text-white"> Vent à {{wind}}km/h - {{windDir}} </h2>
         <h2 class="text-3xl text-center text-white"> Humidité: {{humidity}}% </h2>
         <h2 class="text-3xl text-center text-white"> {{region}} - {{localtime}} </h2>
-        <h2 class="text-3xl text-center text-white"> Dernière update: {{last_updated}} </h2>
+        <h2 class="text-3xl text-center text-white"> Dernière update: {{last_updated}} </h2> 
+
       </div>
     </div>
+    
+    <footer class="flex justify-between w-auto p-4 border-t-4 border-blue-500 bg-gradient-to-t from-yellow-300 via-blue-300 to-red-300 h-20">
+        <h3 class="w-full text-3x1 text-left text-black-300">
+        ceci est un footer (normalement)
+        </h3>
+    </footer>
+
   </div>
   
 </template>
 
 <script>
+import Burger from '@/components/Menu/Burger.vue';
+import Bar from '@/components/Menu/Bar.vue';
+
 class city {
   constructor(name, img) {
     this.name = name,
@@ -49,13 +65,19 @@ let dieppe = new city("Dieppe", require("@/assets/city/dieppe.jpg"))
 let paris = new city("Paris", require("@/assets/city/paris.jpg"))
 let bourges = new city("Bourges", require("@/assets/city/bourges.jpg"))
 let lehavre = new city("Le Havre", require("@/assets/city/lehavre.jpg"))
+let lisboa = new city("Lisboa", require("@/assets/city/lisbonne.jpg"))
+let nauplie = new city("Nafplio", require("@/assets/city/nauplie.jpg"))
 
 export default {
+  components: {
+   Burger,
+   Bar
+ },
   data() {
     return {
       loading: true,
       cityfocused: null,
-      tab: [dieppe, bourges, paris, lehavre],
+      tab: [dieppe, bourges, paris, lehavre, lisboa, nauplie],
       temp: 0,
       wind: 0,
       windDir: "",
@@ -63,6 +85,7 @@ export default {
       humidity: 0,
       localtime: "",
       region: "",
+      country: "",
     }
   },
   methods: {
@@ -78,6 +101,7 @@ export default {
       this.humidity= res.current.humidity
       this.localtime= res.location.localtime
       this.region= res.location.region
+      this.country=res.location.country
       console.log(this.loading)
       }
       this.loading = false
@@ -93,7 +117,8 @@ export default {
   mounted: function () {
     console.log('Mounting')
     this.focusto(dieppe)
-}
+  }
+
 }
 
 </script>
@@ -105,4 +130,5 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+
 </style>
